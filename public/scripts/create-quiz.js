a906709cdfe5ff2ae5a5195d3036d35fc4836851
Donnell Harris <donnell.harris@koodomobile.com>
2023-01-05 19:05:44 -0800

    fix quiz categories redirection

--- public/scripts/create-quiz.js
+++ public/scripts/create-quiz.js
@@ -131,11 +131,18 @@ $(()=>{
 
     window.location.href = "/quiz";
   });
+  
+  $(":button[name='create-quiz']").on('click', function(event) {
+    event.preventDefault();
+
+    window.location.href = '/quiz/create';
+  });
+
 
   const $myQuizzes = $("<button name='MyQuizzes' class='nav-button my-quizzes'>My Quizzes</button>");
 
-   //check cookie for login
-   if (Cookies.get("user_id")) {
+  //check cookie for login
+  if (Cookies.get("user_id")) {
 
     $(".login").html('Logout');
 
@@ -146,7 +153,6 @@ $(()=>{
     $(".login").html('Login');
   }
 
-
   $('.nav-options').on('click','.login', function(event) {
     event.preventDefault();
 
@@ -154,12 +160,10 @@ $(()=>{
 
       $(".my-quizzes").remove();
       Cookies.remove('user_id');
-
-      return window.location.href = '/quiz';
+      $(".login").html('Login');
 
     } else {
 
-      // set user login
       Cookies.set('user_id', 3);
       $(".login").html('Logout');
       $(".nav-options").prepend($myQuizzes);
